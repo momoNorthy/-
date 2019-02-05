773cd4dae10ca301aaea14faff77abb7e9debaa4
momoNorthy <northy360@126.com>
2019-02-05 13:42:54 +0800

    ccccccc-play a joke with 'c'

---
+++ 宏C++.cc
@@ -0,0 +1,21 @@
+
+#define cc <iostream>
+#define ccc int
+#define cccc main
+#define ccccc ()
+#define cccccc {
+#define ccccccc std::cout
+#define cccccccc <<
+#define ccccccccc "hello world!"
+#define cccccccccc std::endl
+#define ccccccccccc ;
+#define cccccccccccc return
+#define ccccccccccccc 0
+#define cccccccccccccc }
+
+#include cc
+ccc cccc ccccc cccccc
+	ccccccc cccccccc ccccccccc cccccccc cccccccccc ccccccccccc
+	cccccccccccc ccccccccccccc ccccccccccc
+cccccccccccccc
+
